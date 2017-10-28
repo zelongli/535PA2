c214bd6ef17e9b2641bd72d4bb366c19759c4912
Li <zelongl@iastate.edu>
2017-10-28 16:53:03 -0500

    1028

--- src/pa2/MinHash.java
+++ src/pa2/MinHash.java
@@ -1,8 +1,48 @@
 package pa2;
 
-public class MinHash {
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 
+public class MinHash {
+	int numDocs;
+	
+	ArrayList<String> allDocsName = new ArrayList<String>(1024);
+	ArrayList<ArrayList<String>> termMatrix = new ArrayList<ArrayList<String>>(1024);
+	Set<String> termSet = new HashSet<String>(); 
+	
 	public MinHash(String folder, int numPermutations){
+		String path = "./docs/";
+		File f = new File(path+folder);
+		File fa[] = f.listFiles();
+		
+		try{
+			Scanner delimiterScanner = new Scanner(fa[0]);
+			
+			
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+		
+		
+//		for(int i = 0; i< fa.length; ++i){
+//			++numDocs;
+//			if(fa[i].isDirectory()){
+//				System.out.println(fa[i].getName()+ "im a directory");
+//			}else{
+//				System.out.println(fa[i].getName()+ "im a file");
+//			}
+//		}
+//		System.out.println("# of docs = " + numDocs);
+	}
+	
+	public void allDocs(){
+		
 		
 	}
+	
+	
 }
--- src/pa2/TestClass.java
+++ src/pa2/TestClass.java
@@ -1,5 +1,7 @@
 package pa2;
 
 public class TestClass {
-
+	public static void main(String args[]){
+		MinHash minHash = new MinHash("space", 1000);		
+	}
 }
