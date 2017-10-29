0ee7b881b10709650918427c336cc2ff5087de56
Li <zelongl@iastate.edu>
2017-10-29 13:41:59 -0500

    1029

--- src/pa2/MinHash.java
+++ src/pa2/MinHash.java
@@ -4,29 +4,34 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class MinHash {
 	int numDocs;
 	
 	ArrayList<String> allDocsName = new ArrayList<String>(1024);
-	ArrayList<ArrayList<Integer>> termMatrix = new ArrayList<ArrayList<Integer>>(1024);
+	ArrayList<HashSet<Integer>> termMatrix = new ArrayList<HashSet<Integer>>(1024);
 	HashMap<String,Integer> termMap = new HashMap<String,Integer>(); 
 	
+	int numP;
+	
 	public MinHash(String folder, int numPermutations){
+		this.numP = numPermutations;
 		String path = "./docs/";
 		File f = new File(path+folder);
 		File fa[] = f.listFiles();
 		
-		for(int i = 0, length = fa.length; i < 10; ++i){
+		for(int i = 0, length = fa.length; i < 5; ++i){
 			try{
 				Scanner delimiterScanner = new Scanner(fa[i]);
 				delimiterScanner.useDelimiter("[\\s,.:;']+");
-				termMatrix.add(new ArrayList<Integer>());
-				
 				
 				allDocsName.add(fa[i].getName());
 				System.out.println(fa[i].getName());
+				termMatrix.add(new HashSet<Integer>());
+							
+				
 				while(delimiterScanner.hasNext()){
 					String temp = delimiterScanner.next();
 					
@@ -34,7 +39,6 @@ public class MinHash {
 						termMap.put(temp, termMap.size());
 					}
 					termMatrix.get(i).add(termMap.get(temp));
-
 				}
 
 				delimiterScanner.close();	
@@ -51,9 +55,25 @@ public class MinHash {
 	}
 	
 	public double exactJaccard(String file1, String file2){
-		System.out.println(allDocsName.indexOf("space-735.txt"));
-		return 1.1;
+		int file1index = allDocsName.indexOf(file1);
+		int file2index = allDocsName.indexOf(file2);
+		
+		HashSet<Integer> intersection = new HashSet<Integer>(termMatrix.get(1));
+		HashSet<Integer> union = new HashSet<Integer>(termMatrix.get(1));
+		
+		intersection.retainAll(termMatrix.get(2));
+		System.out.println("intersection size : " + intersection.size());
+		
+		union.addAll(termMatrix.get(2));
+		System.out.println("union size : " + union.size());
+
+		return (double)intersection.size()/(double)union.size();
+		
 	}
 	
+	public int[] minHashSig(){
+		
+		return null;
+	}
 	
 }
--- src/pa2/TestClass.java
+++ src/pa2/TestClass.java
@@ -3,6 +3,7 @@ package pa2;
 public class TestClass {
 	public static void main(String args[]){
 		MinHash minHash = new MinHash("space", 1000);		
-		minHash.exactJaccard("alad", "sb");
+		
+		System.out.println(minHash.exactJaccard("space-338.txt", "space-339.txt"));
 	}
 }
