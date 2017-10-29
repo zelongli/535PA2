b4b063b163846b60342f11875b16808509a27650
Zelong <zelongli@outlook.com>
2017-10-29 16:07:29 -0500

    1029-1

--- src/pa2/MinHash.java
+++ src/pa2/MinHash.java
@@ -5,36 +5,41 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Random;
 import java.util.Scanner;
 
 public class MinHash {
 	int numDocs;
+	int[][] valueAB;
+	int prime;
 	
-	ArrayList<String> allDocsName = new ArrayList<String>(1024);
-	ArrayList<HashSet<Integer>> termMatrix = new ArrayList<HashSet<Integer>>(1024);
+	ArrayList<String> allDocsName = new ArrayList<String>(1000);
+	ArrayList<HashSet<Integer>> termMatrix = new ArrayList<HashSet<Integer>>(1000);
 	HashMap<String,Integer> termMap = new HashMap<String,Integer>(); 
+	private int[][] minHashMatrix;
 	
-	int numP;
+	int numPmt;
 	
 	public MinHash(String folder, int numPermutations){
-		this.numP = numPermutations;
+		this.numPmt = numPermutations;
 		String path = "./docs/";
 		File f = new File(path+folder);
-		File fa[] = f.listFiles();
+		File farray[] = f.listFiles();
+		setABP();
 		
-		for(int i = 0, length = fa.length; i < 5; ++i){
+		for(int i = 0, length = farray.length; i < 5; ++i){
 			try{
-				Scanner delimiterScanner = new Scanner(fa[i]);
+				Scanner delimiterScanner = new Scanner(farray[i]);
 				delimiterScanner.useDelimiter("[\\s,.:;']+");
 				
-				allDocsName.add(fa[i].getName());
-				System.out.println(fa[i].getName());
-				termMatrix.add(new HashSet<Integer>());
-							
+				allDocsName.add(farray[i].getName());
+				System.out.println(farray[i].getName());
 				
+				termMatrix.add(new HashSet<Integer>());
+											
 				while(delimiterScanner.hasNext()){
 					String temp = delimiterScanner.next();
-					
+					temp.toLowerCase();
 					if(termMap.get(temp) == null){
 						termMap.put(temp, termMap.size());
 					}
@@ -71,9 +76,92 @@ public class MinHash {
 		
 	}
 	
-	public int[] minHashSig(){
+	public int[] minHashSig(String fileName){
+		int index = allDocsName.indexOf(fileName);
+	
+		int[] docMinHashSig = new int[numPmt];
 		
-		return null;
+		for(int i = 0; i < numPmt; ++i){
+			int currentMin = prime;
+			for(Integer element : termMatrix.get(index)){
+				if (currentMin > (valueAB[i][0]*element+valueAB[i][1])%prime){
+					currentMin = (valueAB[i][0]*element+valueAB[i][1])%prime;
+				}
+			}
+			docMinHashSig[i] = currentMin;
+		}
+		return docMinHashSig;
+	}
+	
+	public int[][] minHashMatrix(){
+		minHashMatrix = new int[allDocsName.size()][numPmt];
+		
+		for(int i = 0; i < allDocsName.size(); ++i){
+//			System.out.println("this doc name " + allDocsName.get(i));
+			minHashMatrix[i] = minHashSig(allDocsName.get(i));
+		}
+
+		return minHashMatrix;
+	}
+	
+	public double approximateJaccard(String file1, String file2){
+//		allDocsName.indexOf(file1);
+//		allDocsName.indexOf(file2);
+		int match = 0;
+		
+		for(int i = 0 ; i < numPmt; ++i){
+			if(minHashMatrix[allDocsName.indexOf(file1)][i] == minHashMatrix[allDocsName.indexOf(file2)][i]){
+				++match;
+			}
+		}
+		
+		return (double)match/(double)numPmt;
+	}
+	
+	
+	public int numTerms(){
+		return termMap.size();
+	}
+	
+	public int numPermutations(){
+		return numPmt;
+	} 
+	
+	//helper functions
+	
+	boolean isPrime(int n) {
+	    //check if n is a multiple of 2
+	    if (n%2==0) return false;
+	    //if not, then just check the odds
+	    for(int i=3;i*i<=n;i+=2) {
+	        if(n%i==0)
+	            return false;
+	    }
+	    return true;
+	}
+	
+	public int nextPrime(int n){
+		if(isPrime(n)){
+			return n;
+		}
+		int m = n;
+		while(!isPrime(m)){
+			m++;
+		}
+		return m;
+	}
+	
+	public void setABP(){
+		prime = nextPrime(termMap.size());
+//		System.out.println(allTerms.size());//test
+//		System.out.println(p);//test
+		valueAB = new int[numPmt][2];
+//		minHash = new int[numPmt][files.length];
+		Random random = new Random();
+		for(int i=0; i<numPmt; i++){
+			valueAB[i][0] = Math.abs(random.nextInt() % prime);
+			valueAB[i][1] = Math.abs(random.nextInt() % prime);
+		}
 	}
 	
 }
--- src/pa2/TestClass.java
+++ src/pa2/TestClass.java
@@ -3,7 +3,11 @@ package pa2;
 public class TestClass {
 	public static void main(String args[]){
 		MinHash minHash = new MinHash("space", 1000);		
-		
-		System.out.println(minHash.exactJaccard("space-338.txt", "space-339.txt"));
+		minHash.minHashMatrix();
+		System.out.println(minHash.exactJaccard("space-339.txt", "space-339.txt"));
+		int[][] erer = new int[2][2];
+		int[] line = {1,2};
+		erer[0]=line;
+		System.out.println("erer is" + erer[0][1]);
 	}
 }
