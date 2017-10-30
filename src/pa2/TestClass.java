423f870d86df7eb0124ee1903f329f5dc735d88a
Li <zelongli@outlook.com>
2017-10-30 02:02:47 -0500

    1030 hurry

--- src/pa2/TestClass.java
+++ src/pa2/TestClass.java
@@ -4,15 +4,16 @@ import java.util.Scanner;
 
 public class TestClass {
 	public static void main(String args[]){
-		MinHash minHash = new MinHash("space", 500);		
-		System.out.println("exact " + minHash.exactJaccard("space-513.txt", "space-512.txt"));
-		minHash.minHashMatrix();
-		System.out.println("approx. " + minHash.approximateJaccard("space-513.txt", "space-512.txt"));
-		
+//		MinHash minHash = new MinHash("space", 500);		
+//		System.out.println("exact " + minHash.exactJaccard("space-513.txt", "space-512.txt"));
+//		minHash.minHashMatrix();
+//		System.out.println("approx. " + minHash.approximateJaccard("space-513.txt", "space-512.txt"));
+//		
 		int[][] erer = new int[2][2];
-		int[] line = {1,2};
+		int[] line = {1,2,3};
 		erer[0]=line;
-//		System.out.println("erer is" + "".length());
+		System.out.println("erer is " + erer[0][2]);
+		System.out.println(1/Math.pow(0.8, 400));
 		
 		Scanner delimiterScanner = new Scanner("tmd;;;::;s'''::;bd").useDelimiter("\\s+|\\;|\\:|\\'");
 //		while(delimiterScanner.hasNext()){
