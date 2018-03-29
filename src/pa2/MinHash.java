d35bc62978e4463da69a5737188113f78d3bbbec
Zelong Li <zelongli@outlook.com>
2018-03-29 12:46:28 -0500

    MinHash.java

--- src/pa2/MinHash.java
+++ src/pa2/MinHash.java
@@ -1,8 +1,10 @@
 package pa2;
 
 public class MinHash {
-
+	String folder;
+	int numofPermuations;
 	public MinHash(String folder, int numPermutations){
-		
+		this.folder = folder;
+		this.numofPermuations = numPermutations;
 	}
 }
